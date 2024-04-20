acb377aeff07d826818422492470f2c938ead9a2
aleadinglight <huutamnguyen2302@gmail.com>
2024-04-20 22:23:11 +0300

    Move scanner to a separated package

--- main.go
+++ main.go
@@ -1,37 +1,23 @@
+// main.go
 package main
 
 import (
 	"fmt"
-	"os"
-	"path"
-	"path/filepath"
+
+	"github.com/aleadinglight/turntable/scanner"
 )
 
 func main() {
-	// Define the directory to scan
-	// You can replace "./" with any directory path
-	const dirToScan = "./sounds"
-	var mp3Files []string
+	const SoundDir = "./sounds"
 
-	// Walk through the directory
-	err := filepath.Walk(dirToScan, func(filePath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Check if the file has an mp3 extension
-		if path.Ext(filePath) == ".mp3" {
-			mp3Files = append(mp3Files, filePath)
-		}
-		return nil
-	})
+	// Scan all mp3 files in the sounds directory
+	mp3Files, err := scanner.ScanMP3Files(SoundDir)
 
 	if err != nil {
-		fmt.Println("Error when scanning", dirToScan, ". Error:", err)
+		fmt.Printf("Error when scanning directory '%s'. Error: %v\n", SoundDir, err)
 		return
 	}
 
-	// Print the overall result length and list of mp3 files
 	fmt.Printf("Found %d mp3 files:\n", len(mp3Files))
 	for _, mp3File := range mp3Files {
 		fmt.Println(mp3File)
---
+++ scanner/scanner.go
@@ -0,0 +1,30 @@
+// scanner/scanner.go
+package scanner
+
+import (
+	"os"
+	"path"
+	"path/filepath"
+)
+
+func ScanMP3Files(dirToScan string) ([]string, error) {
+	var mp3Files []string
+
+	err := filepath.Walk(dirToScan, func(filePath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Check if the file has an mp3 extension
+		if path.Ext(filePath) == ".mp3" {
+			mp3Files = append(mp3Files, filePath)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return mp3Files, nil
+}
