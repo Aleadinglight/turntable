5ce4738f9fbb5412790e7a3bca4d5df349935858
aleadinglight <huutamnguyen2302@gmail.com>
2024-05-06 01:15:27 +0300

    Add pid process method for stopping song

--- player/player.go
+++ player/player.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -15,6 +17,7 @@ var (
 func PlayMP3(mp3 string) error {
 	lock.Lock()
 	defer lock.Unlock()
+
 	// Check if mpg123 command is available
 	_, err := exec.LookPath("mpg123")
 	if err != nil {
@@ -35,6 +38,12 @@ func PlayMP3(mp3 string) error {
 		return fmt.Errorf("failed to play MP3: %w", err)
 	}
 
+	// Write PID to a file
+	err = os.WriteFile("/tmp/mpg123.pid", []byte(fmt.Sprintf("%d", cmd.Process.Pid)), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write PID file: %w", err)
+	}
+
 	return nil
 }
 
@@ -42,15 +51,43 @@ func Stop() error {
 	lock.Lock()
 	defer lock.Unlock()
 
-	if cmd != nil && cmd.Process != nil {
-		// Send an interrupt signal to the process
-		err := cmd.Process.Signal(os.Interrupt)
-		if err != nil {
-			return fmt.Errorf("failed to stop MP3: %w", err)
-		}
-		cmd = nil
-		return nil
+	// Read PID from file
+	pidData, err := os.ReadFile("/tmp/mpg123.pid")
+	if err != nil {
+		return fmt.Errorf("failed to read PID file: %w", err)
 	}
 
-	return fmt.Errorf("no song is currently playing")
+	pid, err := strconv.Atoi(string(pidData))
+	if err != nil {
+		return fmt.Errorf("invalid PID: %w", err)
+	}
+
+	if !isMPG123Process(pid) {
+		return fmt.Errorf("no mpg123 process with PID: %d", pid)
+	}
+
+	// Send signal to the process with the given PID
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return fmt.Errorf("failed to find process: %w", err)
+	}
+
+	err = process.Signal(os.Interrupt)
+	if err != nil {
+		return fmt.Errorf("failed to stop MP3: %w", err)
+	}
+
+	return nil
+}
+
+func isMPG123Process(pid int) bool {
+	cmdPath := fmt.Sprintf("/proc/%d/cmdline", pid)
+	cmdline, err := os.ReadFile(cmdPath)
+	if err != nil {
+		// The process might have terminated
+		return false
+	}
+
+	// Check if the cmdline contains the name of the mpg123 binary
+	return strings.Contains(string(cmdline), "mpg123")
 }
