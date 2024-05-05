efe087ff55ff69a5517294e46b8c7551304c8886
Tam Nguyen <huutamnguyen2302@gmail.com>
2024-05-05 06:32:30 +0300

    Add CLI interface for the application (#2)
    * Add cobra for cli app
    
    * Config can be update for user
    
    * Refactor files
    
    * Add command line support
    
    * Add cli option to play a song
    
    * Add os path support to config

--- config/config.go
+++ config/config.go
@@ -1,4 +1,26 @@
 package config
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 // MusicDir is the directory for reading and writing data
-const MusicDir = "~/Music"
+var MusicDir string
+
+func init() {
+	// Get the user's home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error getting home directory:", err)
+		return
+	}
+
+	// Construct the full path to the Music directory
+	MusicDir = filepath.Join(homeDir, "Music")
+}
+
+func Update(newDir string) {
+	MusicDir = newDir
+}
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,4 +1,3 @@
-// scanner/scanner.go
 package scanner
 
 import (
